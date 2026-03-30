19243e8116819214e8a3c2423ad1cf97db83a412
Andre Ryan <andre.soares12@aluno.ce.gov.br>
2026-03-30 13:36:28 +0000

    feito!

---
+++ 1-bimestre/programacao-web/ex15-trabalhando-com-eventos/src/js/script.js
@@ -0,0 +1,14 @@
+
+const form = document.getElementById("formulario");
+const nome = document.getElementById("nome");
+const idade = document.getElementById("idade");
+const resultado = document.getElementById("resultado");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); // Evita o recarregamento da página
+
+  const nomeValor = nome.value;
+  const idadeValor = idade.value;
+
+  resultado.textContent = `Olá ${nomeValor}, você tem ${idadeValor} anos`;
+});
