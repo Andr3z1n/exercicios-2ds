4187e6e13cfd4994912ee557d2d2f050b7efd9e6
Andre Ryan <andre.soares12@aluno.ce.gov.br>
2026-05-04 22:46:42 +0000

    feito!!

---
+++ 2-bimestre/programacao-web/projetos/projeto5-primeiro-site-com-react/nivel2/react/src/pages/Contato.jsx
@@ -0,0 +1,9 @@
+export default function Contato() {
+    return (
+        <div>
+            <h2>Contato</h2>
+            <p>contato@site.com</p>
+            <p>8582394022</p>
+        </div>
+    )
+}
---
+++ 2-bimestre/programacao-web/projetos/projeto5-primeiro-site-com-react/nivel2/react/src/pages/Sobre.jsx
@@ -0,0 +1,9 @@
+export default function Sobre() {
+    return(
+        <div>
+            <h2>Sobre</h2>
+            <p>muito massa, meu primeiro site!</p>
+            <p>Somos uma empresa focada em tecnologia.</p>
+        </div>
+    )
+}
