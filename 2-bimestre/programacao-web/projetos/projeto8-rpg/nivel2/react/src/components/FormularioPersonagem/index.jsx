dd5fe9cdab922e6e80b64e70b46b54ca15b385a9
Andre Ryan <andre.soares12@aluno.ce.gov.br>
2026-06-16 14:53:26 +0000

    Feito!!

--- 2-bimestre/programacao-web/projetos/projeto8-rpg/nivel2/react/src/components/FormularioPersonagem/index.jsx
+++ 2-bimestre/programacao-web/projetos/projeto8-rpg/nivel2/react/src/components/FormularioPersonagem/index.jsx
@@ -1,25 +1,45 @@
+import { useState } from "react"
 import "./style.css"
 
-export default function FormularioPersonagem({ personagens = [], onAdd }) {
-  if (personagens.length === 0) {
-    return (
-      <div className="lista">
-        <p>Nenhum personagem cadastrado.</p>
-      </div>
-    )
+export default function FormularioPersonagem({
+  adicionarPersonagem,
+}) {
+  const [nome, setNome] = useState("")
+  const [classe, setClasse] = useState("")
+
+  function handleSubmit(event) {
+    event.preventDefault()
+
+    if (!nome || !classe) return
+
+    adicionarPersonagem({
+      nome,
+      classe,
+    })
+
+    setNome("")
+    setClasse("")
   }
 
   return (
-    <div className="lista">
-      <ul>
-        {personagens.map((personagem, index) => (
-          <li key={index}>
-            {personagem.nome} - {personagem.classe}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Nome do personagem"
+        value={nome}
+        onChange={(e) => setNome(e.target.value)}
+      />
+
+      <input
+        type="text"
+        placeholder="Classe do personagem"
+        value={classe}
+        onChange={(e) => setClasse(e.target.value)}
+      />
+
+      <button type="submit">
+        Cadastrar Personagem
+      </button>
+    </form>
   )
 }
-
-export default ListaPersonagens
