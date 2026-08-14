ded8e6c5db538449ba41be1e42eb9a98046fb8d8
Andre Ryan <andre.soares12@aluno.ce.gov.br>
2026-08-14 20:36:18 +0000

    verificando ainda

---
+++ 3-bimestre/orientacao-a-objetos/aula07/index.js
@@ -0,0 +1,11 @@
+class Produto {
+    constructor(nome, preco, categoria) {
+        this.nome
+        this.preco
+        this.categoria
+    }
+}
+
+const Produto = new Produto("Notebook", 3500, "Eletrônicos")
+
+module.exports = Produto
